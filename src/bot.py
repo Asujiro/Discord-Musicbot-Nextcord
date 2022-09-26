ec8510260f98206a96fab8ac173ce05ab96b50c6
Asujiro <dohimaru@dohimaru.de>
2022-09-26 21:31:57 +0200

    Added cog load comands
    Exeptions need to be added
    
    Signed-off-by: Asujiro <dohimaru@dohimaru.de>

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
+from distutils import extension
 import os
 import nextcord
 from nextcord.ext import commands
+from nextcord import Interaction, Embed, SlashOption
 from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
@@ -21,6 +23,33 @@ for filename in os.listdir(dirname + "/cogs"):
         if filename.endswith('.py'):
             bot.load_extension(f"cogs.{filename[:-3]}")
 
+
+@nextcord.slash_command(name="load", description="reload cog")
+async def reload_cog(self, interaction: Interaction, *, extention: str = SlashOption(description="cog name")):
+    if interaction.user.id == 218427048153055233:
+        bot.load_extension(f"cogs.{extention}")
+        await interaction.response.send_message("Loaded Cog!")
+    else:
+        await interaction.response.send_message("You need to be an Aplication Admin!")
+
+@nextcord.slash_command(name="unload", description="unload cog")
+async def unload_cog(self, interaction: Interaction, *, extention: str = SlashOption(description="cog name")):
+    if interaction.user.id == 218427048153055233:
+        bot.unload_extension(f"cogs.{extention}")
+        await interaction.response.send_message("Unloaded Cog!" + extention)
+    else:
+        await interaction.response.send_message("You need to be an Aplication Admin!")
+
+@nextcord.slash_command(name="reload", description="reload cog")
+async def reload_cog(self, interaction: Interaction, *, extention: str = SlashOption(description="cog name")):
+    if interaction.user.id == 218427048153055233:
+        bot.reload_extension(f"cogs.{extention}")
+        await interaction.response.send_message("Reloaded Cog!")
+    else:
+        await interaction.response.send_message("You need to be an Aplication Admin!")
+
+
+
 #Welcome Message for my server
 @bot.event
 async def on_member_join(member):
@@ -29,7 +58,7 @@ async def on_member_join(member):
     my_server = 390194259405438989
     if guild.system_channel is not None and guild_id == my_server:
         msg = f"da kommen ja noch mehr affen rein in den discord, cool hallo {member.mention}\nich hab schon viele sachen über dich gehört\nmostly flame von lukas und den anderen, aber das ist ja normal von denen ^^"
-        await guild.system_channel.send(msg)    
+        await guild.system_channel.send(msg)
 
 #Console message on start up
 @bot.event
