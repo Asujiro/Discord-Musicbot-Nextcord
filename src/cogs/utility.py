a739549f521059610efaab76ff29cdf7dc497fe1
Asujiro <lucafischer@live.de>
2023-11-22 18:04:27 +0100

    added edit for twitter.com better comands description and print at start

--- src/cogs/utility.py
+++ src/cogs/utility.py
@@ -1,21 +1,24 @@
-import os
-import re
-from dotenv import load_dotenv, find_dotenv
+
 import nextcord
-import lavalink
 from nextcord.ext import commands
-from lavalink.filters import LowPass
 from nextcord import Interaction, Embed, SlashOption
+
+
 class Utility(commands.Cog):
 
     @nextcord.slash_command(name="vx", description="convert x.com to vxtwitter.com")
-    async def play(self, interaction: Interaction, *, original_url: str = SlashOption(description="x.com")):
+    async def play(self, interaction: Interaction, *, link: str = SlashOption(description="x.com, twitter.com")):
 
-        if "x.com" in original_url:
-            modified_url = original_url.replace("x.com", "vxtwitter.com")
-            print(modified_url)
+        if "x.com" in link:
+            modified_url = link.replace("x.com", "vxtwitter.com")
+            return await interaction.response.send_message(modified_url)
+        elif "twitter.com" in link:
+            modified_url = link.replace("twitter.com", "vxtwitter.com")
+            return await interaction.response.send_message(modified_url)
         else:
-            return await interaction.response.send_message("The URL does not contain 'x.com'. No replacement performed.")
+            return await interaction.response.send_message(
+                "The URL does not contain 'x.com' or 'twitter.com'. No replacement performed.")
+
 
 def setup(bot):
     bot.add_cog(Utility(bot))
